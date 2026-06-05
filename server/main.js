b1e7c64dacd0a9eef6fdd76c49e4618dce40921f
Romanna <romannabric@gmail.com>
2026-06-05 11:53:54 +0200

    Set up basic messages publication and display

--- server/main.js
+++ server/main.js
@@ -1,16 +1,47 @@
 import { Meteor } from "meteor/meteor";
+import { MessagesCollection } from "../imports/MessagesCollection";
+import { check } from "meteor/check";
 
 const ALLOWED_USERS = ["userone", "usertwo", "receiver"];
 
 function isAllowedConversation(from, to) {
-  return;
-  (from === "userone" && to === "receiver") ||
+  return (
+    (from === "userone" && to === "receiver") ||
     (from === "usertwo" && to === "receiver") ||
     (from === "receiver" && to === "userone") ||
-    (from === "receiver" && to === "usertwo");
+    (from === "receiver" && to === "usertwo")
+  );
 }
 
-Meteor.publish("massage", function (user, massage) {});
+Meteor.startup(async () => {
+  if ((await MessagesCollection.find().countAsync()) === 0) {
+    await MessagesCollection.insertAsync({
+      from: "userone",
+      to: "receiver",
+      text: "lalala",
+      createdAt: new Date(),
+    });
+    await MessagesCollection.insertAsync({
+      from: "usertwo",
+      to: "receiver",
+      text: "tututtu",
+      createdAt: new Date(),
+    });
+  }
+});
+
+// Meteor.publish("massage", function (user) {
+//   if (!user) {
+//     throw new Meteor.Error("user-error", "user-error");
+//   }
+//   if (!ALLOWED_USERS.includes(user)) {
+//     throw new Meteor.Error("user-error", "user not found");
+//   }
+// });
+
+Meteor.publish("messages", function () {
+  return MessagesCollection.find();
+});
 
 Meteor.methods({
   async "messages.send"({ from, to, text }) {
